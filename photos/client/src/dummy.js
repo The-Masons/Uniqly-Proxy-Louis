92d94776c4cc5cdeef16bdae6fd3e602708f8eec
lhmun <louis.mun@gmail.com>
2018-04-23 22:42:44 -0700

    Implemented reviews & image carousel displaying some-what

---
+++ photos/client/src/dummy.js
@@ -0,0 +1,61 @@
+const unsplashPhotos = [
+  {
+    name: "mehdizadeh",
+    imageUrl: "https://source.unsplash.com/t3zrEm88ehc",
+    description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus; Videsne quam sit magna dissensio? Quae cum dixisset paulumque institisset, Quid est? Pisone in eo gymnasio, quod Ptolomaeum vocatur, unaque nobiscum Q. Duo Reges: constructio interrete. Si longus, levis dictata sunt. Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia. Sed haec ab Antiocho, familiari nostro, dicuntur multo melius et fortius, quam a Stasea dicebantur. Non dolere, inquam, istud quam vim habeat postea videro;"
+  },
+
+  {
+    name: "otieno",
+    imageUrl: "https://source.unsplash.com/DwTO1h0D4b4",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "javier_reyes",
+    imageUrl: "https://source.unsplash.com/qi2cD5oR_fE",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "roque",
+    imageUrl: "https://source.unsplash.com//25CbufPYkZs",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "ali",
+    imageUrl: "https://source.unsplash.com/CNqUCd6-wx8",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "joanna_nox",
+    imageUrl: "https://source.unsplash.com/I45gK7ZU5ss",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "rawpixel",
+    imageUrl: "https://source.unsplash.com/izTZ-TtdwCs",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "brooke",
+    imageUrl: "https://source.unsplash.com/54onJzthC_g",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "akakak",
+    imageUrl: "https://source.unsplash.com/Zjo2Va5AkaQ",
+    description: "Quae cum dixisset paulumque institisset, Quid est? Pisone in eo gymnasio, quod Ptolomaeum vocatur, unaque nobiscum Q. Duo Reges: constructio interrete. Si longus, levis dictata sunt. Atque haec ita iustitiae propria sunt, ut sint virtutum reliquarum communia. "
+  },
+
+  {
+    name: "jbriscoe",
+    imageUrl: "https://source.unsplash.com/Pn0XP2Klbcw",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+  {
+    name: "daniel",
+    imageUrl: "https://source.unsplash.com/X6aSVG58d2o",
+    description: "Frater et T. Nam adhuc, meo fortasse vitio, quid ego quaeram non perspicis. Quamquam haec quidem praeposita recte et reiecta dicere licebit."
+  },
+];
+
+export default unsplashPhotos
---
+++ reviews/client/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ReviewList from './components/ReviewList.jsx';
+
+var App = () =>{
+   return <ReviewList/>
+}
+
+ReactDOM.render(<App />, document.getElementById('Reviews'));
